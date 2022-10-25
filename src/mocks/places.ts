774b8f2d1e6c83e07820ab2e836990df1242d00e
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2022-10-25 17:57:49 -0300

    test: should marker in correct places

---
+++ src/mocks/places.ts
@@ -0,0 +1,23 @@
+const places = [
+  {
+    id: '1',
+    name: 'Petrópolis',
+    slug: 'petropolis',
+    position: {
+      latitude: 46.88,
+      longitude: -0.567,
+    },
+  },
+
+  {
+    id: '2',
+    name: 'Belém',
+    slug: 'belém',
+    position: {
+      latitude: 24.88,
+      longitude: -1.234,
+    },
+  },
+];
+
+export default places;
